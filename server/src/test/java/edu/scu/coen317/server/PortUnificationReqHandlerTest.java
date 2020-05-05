7cfbfc4d82cff438c998a1c7cf504d572ec96df6
Huiyi Li <huiyi.li@outlook.com>
2020-05-04 20:27:54 -0700

    [master] remove unused import

--- server/src/test/java/edu/scu/coen317/server/PortUnificationReqHandlerTest.java
+++ server/src/test/java/edu/scu/coen317/server/PortUnificationReqHandlerTest.java
@@ -10,8 +10,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import javax.sound.sampled.Port;
-
 import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnit4.class)
